7a1f158f1777a3043cb69c6ab6fc998e3797387f
Miles Cramer <02miles.cramer@gulllakecs.org>
2019-10-24 10:51:01 -0400

    Initial commit for loop + while loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -11,3 +11,10 @@ for i in range(3):
     print('Outer_for_Loop: ' + str(i))
     for l in range(2):
         print("     inner for loop: " + str(l))
+
+'''
+Programmer: Miles
+Date: 10/24/19
+Program: For loop + While loop
+
+This program will create a for loop with a while loop embedded into it'''
