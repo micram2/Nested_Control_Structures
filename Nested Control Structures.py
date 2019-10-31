972c0d85f7dc99a2e826211f8c0787d8f2dfbad3
micram2 <56842261+micram2@users.noreply.github.com>
2019-10-31 13:54:42 -0400

    Add our outer for loop and inner while loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -18,3 +18,10 @@ Date: 10/24/19
 Program: For loop + While loop
 
 This program will create a for loop with a while loop embedded into it'''
+
+for i in range(4):
+    print("Outer for loop: " + str(i))
+    x = 6
+    while x >= 0:
+        print("    While loop: + str(x))
+        x = x - 1
